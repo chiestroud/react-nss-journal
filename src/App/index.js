b0065c994e8a660f7f5b26427dd364bb516a1454
Chie Stroud <urirunrunkoko@gmail.com>
2021-05-03 21:58:32 -0500

    added authentication

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
 import './App.scss';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          username: authed.email.split('@')[0]
+        };
+        setUser(userInfoObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <Router>
-      <NavBar />
-      <Routes />
+      <NavBar user={user}/>
+      <Routes user={user}/>
     </Router>
   );
 }
--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Home from '../views/Home';
 import JanApr from '../views/JanApp';
 import May from '../views/May';
 
-export default function Routes() {
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  const routeChecker = (taco) => (user
+    ? (<Component {...taco} user={user} />)
+    : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any
+};
+export default function Routes({ user }) {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={May}/>
-        <Route path='/may' component={May}/>
-        <Route path='/jan-apr' component={JanApr}/>
+        <Route exact path='/' component={() => <Home user={user}/>}/>
+        <PrivateRoute
+          path='/may'
+          user={user}
+          component={May} />
+        <PrivateRoute
+          path='/jan-apr'
+          user={user}
+          component={JanApr} />
       </Switch>
     </div>
   );
 }
+
+Routes.propTypes = {
+  user: PropTypes.any
+};
